a3d403db8cb1e5c6f1ca809c12078c949f5a239d
KPkamil <kamil.pawlak51@gmail.com>
2024-08-05 21:31:28 +0200

    Create HowItWorks section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Model from "@components/Model";
 import Navbar from "@components/Navbar";
 import Features from "@components/Features";
 import Highlights from "@components/Highlights";
+import HowItWorks from "@components/HowItWorks";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
     </main>
   );
 };
