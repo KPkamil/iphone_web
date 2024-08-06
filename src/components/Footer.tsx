7f64e8580d91df7a445231bf40f680248524a35c
KPkamil <kamil.pawlak51@gmail.com>
2024-08-06 13:55:47 +0200

    Improve footer appearance on small devices

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,27 +11,26 @@ const Footer = () => {
             or <span className="underline text-blue">other retailer</span> near
             you.
           </p>
-          <p className="font-semibold text-gray text-xs">
+          <p className="font-semibold text-gray text-xs mt-2">
             Or call 1-800-MY-APPLE.
           </p>
         </div>
         <div className="bg-neutral-700 my-5 h-[1px] w-full">
           <div className="flex md:flex-row flex-col md:items-center justify-between">
-            <p className="font-semibold text-gray text-xs">
+            <p className="font-semibold text-gray text-xs mt-2">
               Copright @ 2024 Apple Inc. All rights reserved.
             </p>
-            <div className="flex">
+            <div className="flex flex-wrap mt-2 gap-y-1">
               {footerLinks.map((link, idx) => (
-                <p
+                <span
                   key={link}
                   className="font-semibold text-gray text-xs cursor-pointer"
                 >
                   {link}
-                  {""}
                   {idx !== footerLinks.length - 1 && (
                     <span className="mx-2"> | </span>
                   )}
-                </p>
+                </span>
               ))}
             </div>
           </div>
